18cdf1516020571c4184d3db57fa5308c1b00b82
Robby <robby-olson11@gmail.com>
2019-11-04 14:08:16 -0800

    added quarter buttons

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [quarter, setQuarter] = useState(1);
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,8 +19,14 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{quarter}</div>
       </div>
+      <div className ="quarterButtons"> 
+          <button onClick = {() => setQuarter(1)} className="quarter1button">Q 1</button>
+          <button onClick = {() => setQuarter(2)} className="quarter2button">Q 2</button>
+          <button onClick = {() => setQuarter(3)} className="quarter3button">Q 3</button>
+          <button onClick = {() => setQuarter(4)} className="quarter4button">Q 4</button>
+        </div>
     </div>
   );
 };
